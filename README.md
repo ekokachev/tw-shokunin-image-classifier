52798cbcc7c71cc6233d3cdab227b3da25c4622e
EC2 Default User <ec2-user@ip-172-31-5-118.ap-southeast-2.compute.internal>
2019-08-04 04:13:37 +0000

    more clear-out

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ https://www.kaggle.com/c/synthetic-image-classification/overview
 # Setup Overview
 * VGG11 as described here (https://medium.com/coinmonks/paper-review-of-vggnet-1st-runner-up-of-ilsvlc-2014-image-classification-d02355543a11) was implemented using Keras (Tensorflow backend) with Dropout & BatchNormalisation layers. Idea to add those layers was borrowed from somewhere. 
 
-* Training was running on AWS EC2 Spot p3.8xlarge instances. Deep learning AMI (ami-38a5525a) was used and tensorflow 3.6 conda environment. 
+* Training was running on AWS EC2 Spot p3.8xlarge instances. Deep learning AMI (ami-38a5525a) &  Tensorflow 3.6 conda environment were used. 
 
-* None of the "criteria of awesomeness" was met :) 
+* None of the "criteria of awesomeness" was met. 
 
 * Hyper parameters tuning wasn't done, so maybe a few additional % points could be squeezed out of this architecture. 
--- eugenenet/vgg11.py
+++ eugenenet/vgg11.py
@@ -12,19 +12,15 @@ from keras import backend as K
 class VGG11:
 	@staticmethod
 	def build(width, height, depth, classes):
-		# initialize the model along with the input shape to be
-		# "channels last" and the channels dimension itself
+
 		model = Sequential()
 		inputShape = (height, width, depth)
 		chanDim = -1
  
-		# if we are using "channels first", update the input shape
-		# and channels dimension
 		if K.image_data_format() == "channels_first":
 			inputShape = (depth, height, width)
 			chanDim = 1
 
-		# CONV => RELU => POOL layer set
 		model.add(Conv2D(64, (3, 3), padding="same",
 			input_shape=inputShape))
 		model.add(Activation("relu"))
@@ -32,7 +28,6 @@ class VGG11:
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Dropout(0.2))
 
-		# (CONV => RELU) * 2 => POOL layer set
 		model.add(Conv2D(128, (3, 3), padding="same"))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
@@ -59,11 +54,10 @@ class VGG11:
 		model.add(BatchNormalization(axis=chanDim))
 		model.add(Conv2D(512, (3, 3), padding="same", activation="relu"))
 		model.add(BatchNormalization(axis=chanDim))
-# 
+
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Dropout(0.2))
 
-		# first (and only) set of FC => RELU layers
 		model.add(Flatten())
 		model.add(Dense(4096, activation='relu'))
 		model.add(BatchNormalization())
@@ -71,14 +65,9 @@ class VGG11:
 		model.add(Dense(1000, activation='relu'))
 		model.add(BatchNormalization())
 		model.add(Dropout(0.5))	
-#
-#		model.add(Activation("relu"))
-#		model.add(BatchNormalization())
-#		model.add(Dropout(0.5))
  
 		# softmax classifier
 		model.add(Dense(classes))
 		model.add(Activation("softmax"))
  
-		# return the constructed network architecture
 		return model
